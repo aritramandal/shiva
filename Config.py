d559a184003a30eb96146ab036064a57123a3cc0
suhaash02 <71298965+suhaash02@users.noreply.github.com>
2020-10-25 07:21:25 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -49,7 +49,7 @@ class Config((object)):
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_LOG_P_M_S = bool(os.environ.get("NO_LOG_P_M_S", True))
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/174268e8434a45c80b905.jpg"
     )
     # Genius lyrics get this value from https://genius.com/developers both has
     # same values
