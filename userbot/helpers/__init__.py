efac7040b49ed4174c8c9ae65262189b26011f09
suhaash02 <71298965+suhaash02@users.noreply.github.com>
2020-10-28 11:24:23 +0530

    Delete __init__.py

--- userbot/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import fonts
-from . import memeshelper as Dextromemes
-from .exceptions import CancelProcess
-from .functions import *
-from .progress import *
-from .qhelper import process
-from .utils import *
